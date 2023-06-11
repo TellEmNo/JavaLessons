bd2c676e85e154c0036a4a90814cf266f4ace38d
Vladislav Verin <dippicaka@gmail.com>
2023-06-12 00:45:09 +0300

    Initial commit

---
+++ src/main/java/HomeWork1/Main.java
@@ -0,0 +1,11 @@
+package HomeWork1;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int k = 2000;
+        int i = new Random().nextInt(k);
+        System.out.println(i);
+    }
+}
